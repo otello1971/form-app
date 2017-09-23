7140210242045928522612568cba7d6054ebf76c
Pablo <otello1971@yahoo.com>
2017-09-23 00:44:42 -0300

    Respaldo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<hero-form></hero-form>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
